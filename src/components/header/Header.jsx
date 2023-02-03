0c339e5c56c569e6adbeaabb4b54d57c7e54579c
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-02-03 09:04:06 +0000

    Recreated the Kalasinga App on replit .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Header.css'
+ import React from 'react'
+ import './Header.css'
+ 
+ function Header() {
+   return (
+      <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">The</span>
+        <span className="headerTitleLg">WHISPERER...</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
 
-function Header() {
-  return (
-    <div className="header">
-      <h1>Hello There</h1>
-    </div>
-  )
-}
-
-export default Header
+</div>
+   )
+ }
+ 
+ export default Header
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import './Write.css'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+function Write() {
+ 
+  return (
+     <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+        
+      />
+    
+      <form className="writeForm"  enctype="multipart/form-data">
+   <div className='writeFormGroup' >
+
+  <select placeholder='Language' id='fileInput' className="form-select  form-select-sm mt-5 w-25" aria-label=".form-select-sm example">
+  <option  selected disabled  > <i> --choose a language-- </i> </option>
+  <option value="1">English</option>
+  <option value="2">Kiswahili</option>
+  <option value="3">Local Dilect</option>
+  </select>
+
+       </div>
+       <div className='writeFormGroup' >
+
+<select placeholder='Category' id='fileInput' className="form-select form-select-sm mt-5 w-25" aria-label=".form-select-sm example">
+<option  selected disabled  > <i> --Category-- </i> </option>
+<option value="1">Music</option>
+<option value="2">Sports</option>
+<option value="3">Politics</option>
+</select>
+
+     </div>
+        <div className="writeFormGroup">
+
+    
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input id="fileInput" type="file" style={{ display: "none" }}  />
+          <input
+            className="writeInput"
+            placeholder="Title"
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story..."
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Write
